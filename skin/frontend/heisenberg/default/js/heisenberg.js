f05973cb84558de89ab43eafc304144f7cdf467c
Hitesh Pachpor <hitesh.pachpor@iksula.com>
2014-01-12 01:17:12 +0530

    Further improvements in heading design, add font icons for all header buttons

--- skin/frontend/heisenberg/default/js/heisenberg.js
+++ skin/frontend/heisenberg/default/js/heisenberg.js
@@ -3,4 +3,7 @@ jQuery(document).ready(function($) {
 	var fontIconElement = '<div class="font-awesome fa fa-shopping-cart"></div>';
 	$('button.btn-cart').prepend(fontIconElement);
 
+	var fontIconElement = '<div class="font-awesome fa fa-sitemap"></div>';
+	$('a.level-top').prepend(fontIconElement);
+
 });
