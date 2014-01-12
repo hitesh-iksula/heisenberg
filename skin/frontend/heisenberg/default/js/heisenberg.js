4fb59a3c2da9f196397e4552c2b72381bc6ad801
Hitesh Pachpor <hitesh.pachpor@iksula.com>
2014-01-13 00:28:52 +0530

    Adopt window height for main so that footer always sticks to bottom

--- skin/frontend/heisenberg/default/js/heisenberg.js
+++ skin/frontend/heisenberg/default/js/heisenberg.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function($) {
 
+	function main_height() {
+		var footer_container = $('.footer-container').outerHeight();
+		var main_inner_offset = parseInt($('.main').css('padding-top')) + parseInt($('.main').css('padding-bottom'));
+		var height_to_exclude = footer_container + main_inner_offset + parseInt($('.main-container').css('padding-top'));
+		var height_to_main = $(window).outerHeight() - height_to_exclude;
+		console.log(main_inner_offset);
+		console.log(parseInt($('.main-container').css('padding-top')));
+		$('.main').css('min-height', height_to_main);
+	}
+
+	main_height();
+	$(window).resize(main_height);
+
 	// var fontIconElement = '<div class="font-awesome fa fa-shopping-cart"></div>';
 	// $('button.btn-cart').prepend(fontIconElement);
 
@@ -17,6 +30,6 @@ jQuery(document).ready(function($) {
 
 	$('.close-drawer').click(function() {
 		$('.header-drawer').slideToggle("fast");
-	})
+	});
 
 });
