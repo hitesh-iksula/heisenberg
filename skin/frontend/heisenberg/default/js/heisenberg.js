a19785c4c720a5226624618209cf9e029d6c5dd5
Hitesh Pachpor <hitesh.pachpor@iksula.com>
2014-01-12 10:19:44 +0530

    Color revamp to grey and dull blue, major design changes in header, product list

--- skin/frontend/heisenberg/default/js/heisenberg.js
+++ skin/frontend/heisenberg/default/js/heisenberg.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
 
-	var fontIconElement = '<div class="font-awesome fa fa-shopping-cart"></div>';
-	$('button.btn-cart').prepend(fontIconElement);
+	// var fontIconElement = '<div class="font-awesome fa fa-shopping-cart"></div>';
+	// $('button.btn-cart').prepend(fontIconElement);
 
-	var fontIconElement = '<div class="font-awesome fa fa-sitemap"></div>';
+	var fontIconElement = '<div class="font-awesome fa fa-bars"></div>';
 	$('a.level-top').prepend(fontIconElement);
 
 });
