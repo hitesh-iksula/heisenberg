7f7d0b120beaf9fa29dfc0478fd1b7f57c1cd2b8
Hitesh Pachpor <hitesh.pachpor@iksula.com>
2014-01-12 23:41:47 +0530

    Introduction of header drawer, show navigation inside drawer. close button

--- skin/frontend/heisenberg/default/js/heisenberg.js
+++ skin/frontend/heisenberg/default/js/heisenberg.js
@@ -6,4 +6,17 @@ jQuery(document).ready(function($) {
 	var fontIconElement = '<div class="font-awesome fa fa-bars"></div>';
 	$('a.level-top').prepend(fontIconElement);
 
+	var html = $('ul.level0').html();
+	$('ul.level0').remove();
+	$('.header-drawer .html').html('<ul class="custom-navigation">' + html + '</ul>');
+
+	$('#nav').click(function(e) {
+		e.preventDefault();
+		$('.header-drawer').slideToggle();
+	});
+
+	$('.close-drawer').click(function() {
+		$('.header-drawer').slideToggle();
+	})
+
 });
